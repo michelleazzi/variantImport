21d2d14bd39a41b3097479b8dd157566b7b2f5c4
michelleazzi <161088917+michelleazzi@users.noreply.github.com>
2024-02-23 16:44:54 -0600

    variantImport.R

--- variantImport.R
+++ variantImport.R
@@ -9,8 +9,3 @@ variantImport <- function(url, filename) {
   data <- data[order(data$chrom, data$begPos, data$endPos), ]
   return(data)
 }
-source("variantImport.R",echo = T)
-q1Url <- "https://utexas.box.com/shared/static/21vasb3e7b57az1ud8ccuqxlqo361cxn.zip"
-q1Filename <- "phalliiFNP_Variants_scrambledSubset.csv"
-varDf <- variantImport(url = q1Url,filename = q1Filename)
-str(varDf,strict.width ="cut")
